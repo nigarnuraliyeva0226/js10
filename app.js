bf8939da9d235a583738eca5b5345b57cec15f2a
nigarnuraliyeva0226 <nikanurali0226@gmail.com>
2023-08-27 16:39:10 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let li1 = document.createElement ("li")
+console.log (li1)
+li1.innerHTML = "Nigar Nuraliyeva"
+let li2 = document.createElement ("li")
+console.log (li2)
+li2.innerHTML = "Ruba Nagiyeva"
+let li3 = document.createElement ("li")
+console.log (li3)
+li3.innerHTML = "Azer Memmedov"
+
